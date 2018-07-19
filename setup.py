f507cc18dd3873e375a19e2efda9272c15a3c59c
Viktor Tatarnikov <viktor@tatarnikov.org>
2018-07-19 11:07:41 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+from datetime import datetime
+
+now = datetime.now()
+
+setup(
+    name='scopuli_core_console',
+    author='Viktor Tatarnikov',
+    author_email='viktor@tatarnikov.org',
+    namespace_packages=['Scopuli'],  # line 8
+    platforms='any',
+    zip_safe=False,
+    version='0.1.{0}.{1}.{2}.{3}.{4}'.format(now.year, now.month, now.day, now.hour, now.minute),
+    license="Apache",
+    url="https://scopuli.tatarnikov.org",
+    packages=['Scopuli', 'Scopuli.Console'],
+    #packages=find_packages(exclude=["tests.*", "tests"]),
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    include_package_data=True,
+)
